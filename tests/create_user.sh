d193e4e0e430db9c11befd615e8586cd9383503e
lordPachy <francescocalzona03@gmail.com>
2025-02-06 18:23:53 +0100

    Rewritten tests

--- tests/create_user.sh
+++ tests/create_user.sh
@@ -2,9 +2,5 @@
 
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n" 
 printf "USER CREATION:\n"
-auth="$(curl -s --header "Content-Type: application/json" --request PUT --data '{"name":"Pippo"}' http://0.0.0.0:3000/session | jq '.identifier')"
-auth=${auth//\"}
+curl -s --header "Content-Type: application/json" --request PUT --data '{"name":"Pippo"}' http://0.0.0.0:3000/session
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
-printf "AUTHENTICATION PRINTING:\n"
-printf $auth
-printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
