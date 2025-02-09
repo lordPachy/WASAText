7eb3b7f04126609995c5aafc0dabab07d7b2bba4
lordPachy <francescocalzona03@gmail.com>
2025-02-09 20:46:07 +0100

    errors while updating

--- webui/src/views/Conversation.vue
+++ webui/src/views/Conversation.vue
@@ -9,11 +9,13 @@ export default {
 			errormsg: null,
 			loading: false,
 			some_data: null,
-			data: null,
+			data: {},
 			isGroup: false,
+			message: "",
+			photo: "",
 		}
 	},
-	mounted() {
+	created() {
 		this.refresh();
 	},
 	methods: {
@@ -23,7 +25,21 @@ export default {
 				try{
 					let response = await this.$axios.get("/conversations/" + this.cid, {headers: {Authorization: this.$router.id}});
 					this.data = response.data;
+					this.messages = response.data.messages;
 					this.isGroup = Object.keys(response.data).length > 3;
+				} catch (e) {
+					this.errormsg = e.toString();
+				}
+				this.loading = false;
+			},
+			async sendMessage() {
+				this.loading = true;
+				this.errormsg = null;
+				try{
+					let response = await this.$axios.post("/conversations/" + this.cid, {content: this.message, replyingto: 0}, {headers: {Authorization: this.$router.id}});
+					this.message = "";
+					this.photo = "";
+					this.refresh();
 				} catch (e) {
 					this.errormsg = e.toString();
 				}
@@ -50,6 +66,12 @@ export default {
       </ul>
     </div>
 	
+    <textarea v-model="message" class="bottom" placeholder="New message" />
+    <div class="btn-group me-2">
+      <button type="button" class="btn btn-sm btn-outline-secondary" @click.stop="sendMessage">
+        Send
+      </button>
+    </div>
     <ErrorMsg v-if="errormsg" :msg="errormsg" />
   </div>
 </template>
