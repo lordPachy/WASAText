d47232273186c9ba51801ec977fc47595f46ddcd
lordPachy <francescocalzona03@gmail.com>
2025-02-11 13:12:18 +0100

    Removed general text on sidebar

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -17,9 +17,6 @@ export default {}
     <div class="row">
       <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
         <div class="position-sticky pt-3 sidebar-sticky">
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-            <span></span>
-          </h6>
           <ul class="nav flex-column">
             <li class="nav-item">
               <RouterLink to="/" class="nav-link">
